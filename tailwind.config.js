8583c1c76c7cf9a33ff47b3c319346252ac75cef
David-dev29 <efrendavid288@gmail.com>
2026-01-29 23:04:31 -0600

    feat: add Poppins typography globally

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
       colors: {
         // Paleta principal - Rojo JoyMap
         primary: {
